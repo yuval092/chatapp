12725163b1d95038bd340ddbfe5acd1a853a7915
Yuval Farkash <yuval.farkash8@gmail.com>
2025-02-14 11:09:36 +0200

    Init commit - from mail

---
+++ Signalir-ChatApp-Ver2/Database.cs
@@ -0,0 +1,113 @@
+﻿using Android.Content;
+using Android.Database.Sqlite;
+using System.Collections.Generic;
+
+namespace Signalir_ChatApp
+{
+
+
+
+    public class DatabaseHelper : SQLiteOpenHelper
+    {
+        private const string chatDatabaseName = "ChatApp.db";
+        private const int DatabaseVersion = 1;
+
+        public DatabaseHelper(Context context)
+            : base(context, chatDatabaseName, null, DatabaseVersion) { }
+
+        // Called when the database is first created
+        public override void OnCreate(SQLiteDatabase db)
+        {
+            db.ExecSQL(@"CREATE TABLE Messages (
+                        MessageId INTEGER PRIMARY KEY AUTOINCREMENT, 
+                        sendingUserName TEXT NOT NULL, 
+                        MessageType TEXT NOT NULL, 
+                        recivingUserName TEXT NOT NULL, 
+                        MessageText TEXT NOT NULL, 
+                        MessageTypeOptions TEXT NOT NULL, 
+                        FilePath TEXT NOT NULL, 
+                        PortraitOrlandscape TEXT NOT NULL, 
+                        Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP);");
+        }
+
+
+        public List<Dictionary<string, string>> GetMessagesForUsers(string user1, string user2)
+        {
+            var messages = new List<Dictionary<string, string>>();
+            var db = this.ReadableDatabase;
+
+            // Query to select all messages where either user1 sent to user2 or user2 sent to user1, sorted by Timestamp
+            string query = @"
+              SELECT * FROM Messages 
+              WHERE (sendingUserName = ? AND recivingUserName = ?) 
+              OR (sendingUserName = ? AND recivingUserName = ?) 
+              ORDER BY Timestamp ASC;";
+
+            var cursor = db.RawQuery(query, new string[] { user1, user2, user2, user1 });
+
+            // Process the result set
+            while (cursor.MoveToNext())
+            {
+                var message = new Dictionary<string, string>
+        {
+            { "MessageId", cursor.GetInt(cursor.GetColumnIndexOrThrow("MessageId")).ToString() },
+            { "sendingUserName", cursor.GetString(cursor.GetColumnIndexOrThrow("sendingUserName")) },
+            { "MessageType", cursor.GetString(cursor.GetColumnIndexOrThrow("MessageType")) },
+            { "recivingUserName", cursor.GetString(cursor.GetColumnIndexOrThrow("recivingUserName")) },
+            { "MessageText", cursor.GetString(cursor.GetColumnIndexOrThrow("MessageText")) },
+            { "MessageTypeOptions", cursor.GetString(cursor.GetColumnIndexOrThrow("MessageTypeOptions")) },
+            { "FilePath", cursor.GetString(cursor.GetColumnIndexOrThrow("FilePath")) },
+            { "PortraitOrlandscape", cursor.GetString(cursor.GetColumnIndexOrThrow("PortraitOrlandscape")) },
+            { "Timestamp", cursor.GetString(cursor.GetColumnIndexOrThrow("Timestamp")) }
+            
+        };
+
+                messages.Add(message);
+            }
+
+            cursor.Close();
+            db.Close();
+
+            return messages;
+        }
+
+
+
+        public List<string> GetUsersFromDatabase()
+        {
+            var db = this.ReadableDatabase;
+
+            var query = @"
+            SELECT DISTINCT sendingUserName AS UserName
+            FROM Messages
+            UNION
+            SELECT DISTINCT recivingUserName AS UserName
+            FROM Messages;
+        ";
+
+            var cursor = db.RawQuery(query, null);
+            var uniqueUsernames = new List<string>();
+
+            while (cursor.MoveToNext())
+            {
+                uniqueUsernames.Add(cursor.GetString(cursor.GetColumnIndexOrThrow("UserName")));
+            }
+
+            cursor.Close();
+            db.Close();
+            return uniqueUsernames;
+        }
+
+
+
+        // Called when the database version changes
+        public override void OnUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            db.ExecSQL("DROP TABLE IF EXISTS Messages;");
+            OnCreate(db);
+        }
+    }
+
+
+
+}
---
+++ Signalir-ChatApp-Ver2/FullScreenPhoto.cs
@@ -0,0 +1,49 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using ImageViews.Photo;
+
+namespace Signalir_ChatApp
+{
+    // Fragment to show image in full-screen
+    public class FullScreenImageFragment : Fragment
+    {
+        private readonly string imagePath;
+
+        public FullScreenImageFragment(string imagePath)
+        {
+            this.imagePath = imagePath;
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Create root layout with MATCH_PARENT to fill the screen
+            var rootLayout = new FrameLayout(Context)
+            {
+                LayoutParameters = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)
+            };
+
+            rootLayout.SetBackgroundColor(Color.Black); // Black background for full-screen effect
+
+            // Create PhotoView to display the image
+            var photoView = new PhotoView(Context)
+            {
+                LayoutParameters = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MatchParent, ViewGroup.LayoutParams.MatchParent)
+                {
+                    Gravity = GravityFlags.Center
+                }
+            };
+
+            // Load image from file path and set it to the PhotoView
+            var bitmap = BitmapFactory.DecodeFile(imagePath);
+            photoView.SetImageBitmap(bitmap);
+
+            // Add the PhotoView to the root layout
+            rootLayout.AddView(photoView);
+
+            return rootLayout;
+        }
+    }
+}
